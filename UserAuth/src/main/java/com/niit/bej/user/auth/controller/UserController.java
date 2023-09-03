9aef30f2f38ad8d6bad236d74ee6be395c7bbf52
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-04 00:06:26 +0530

    added registerNewUser() to UserController.java

--- UserAuth/src/main/java/com/niit/bej/user/auth/controller/UserController.java
+++ UserAuth/src/main/java/com/niit/bej/user/auth/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.niit.bej.user.auth.controller;
 
+import com.niit.bej.user.auth.exception.UserAlreadyExistsException;
+import com.niit.bej.user.auth.model.User;
 import com.niit.bej.user.auth.service.UserService;
 import com.niit.bej.user.auth.service.security.SecurityTokenGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +22,15 @@ public class UserController {
     public UserController(UserService userService, SecurityTokenGenerator securityTokenGenerator) {
         this.userService = userService;
         this.securityTokenGenerator = securityTokenGenerator;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerNewUser(@RequestBody User user) {
+        try {
+            User registeredUser = this.userService.registerUser(user);
+            return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
+        } catch (UserAlreadyExistsException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
+        }
     }
 }
