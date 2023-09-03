5d3fa4b59e989a01eeba9f2985bb60a7331812d3
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-03 23:48:59 +0530

    added @ResponseStatus annotation to InvalidCredentialsException.java

--- UserAuth/src/main/java/com/niit/bej/user/auth/exception/InvalidCredentialsException.java
+++ UserAuth/src/main/java/com/niit/bej/user/auth/exception/InvalidCredentialsException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.user.auth.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "Email or Password incorrect!", code = HttpStatus.CONFLICT)
 public class InvalidCredentialsException extends Exception {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
 }
