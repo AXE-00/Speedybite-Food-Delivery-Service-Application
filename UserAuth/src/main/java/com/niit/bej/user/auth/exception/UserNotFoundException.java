16207a9810b5f5a2351cc4d2fd2cd4aed997c543
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-03 23:50:32 +0530

    added @ResponseStatus annotation to UserNotFoundException.java

--- UserAuth/src/main/java/com/niit/bej/user/auth/exception/UserNotFoundException.java
+++ UserAuth/src/main/java/com/niit/bej/user/auth/exception/UserNotFoundException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.user.auth.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "User not found in the database!", code = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
