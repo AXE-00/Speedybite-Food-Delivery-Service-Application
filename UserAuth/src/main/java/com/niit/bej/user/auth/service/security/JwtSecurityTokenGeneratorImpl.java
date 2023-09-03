50c9a9caad49babcf6d9b05e2267514b34fcde74
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-03 23:57:29 +0530

    added fields to JwtSecurityTokenGeneratorImpl.java

--- UserAuth/src/main/java/com/niit/bej/user/auth/service/security/JwtSecurityTokenGeneratorImpl.java
+++ UserAuth/src/main/java/com/niit/bej/user/auth/service/security/JwtSecurityTokenGeneratorImpl.java
@@ -5,6 +5,11 @@ import com.niit.bej.user.auth.model.User;
 import java.util.Map;
 
 public class JwtSecurityTokenGeneratorImpl implements SecurityTokenGenerator {
+    public static final int SECONDS = 60;
+    public static final int MINUTES = 60;
+    public static final int DURATION = 10;
+    public static final int MILLISECOND = 1000;
+
     @Override
     public Map<String, String> generateToken(User user) {
         return null;
