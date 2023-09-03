2d663b558f0a4d6ccce7d16b7351336b6a2013bf
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-03 23:33:35 +0530

    added @Repository annotation to UserRepository.java

--- UserAuth/src/main/java/com/niit/bej/user/auth/repository/UserRepository.java
+++ UserAuth/src/main/java/com/niit/bej/user/auth/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.niit.bej.user.auth.repository;
 
 import com.niit.bej.user.auth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findUserByEmail(String email);
 }
