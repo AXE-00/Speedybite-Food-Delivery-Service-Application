654e6560d9f03d6784756f12665462017fcfcf21
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-03 23:52:17 +0530

    added @Service annotation; injected field dependency of userRepository to UserServiceImpl.java

--- UserAuth/src/main/java/com/niit/bej/user/auth/service/UserServiceImpl.java
+++ UserAuth/src/main/java/com/niit/bej/user/auth/service/UserServiceImpl.java
@@ -4,8 +4,20 @@ import com.niit.bej.user.auth.exception.InvalidCredentialsException;
 import com.niit.bej.user.auth.exception.UserAlreadyExistsException;
 import com.niit.bej.user.auth.exception.UserNotFoundException;
 import com.niit.bej.user.auth.model.User;
+import com.niit.bej.user.auth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public User registerUser(User user) throws UserAlreadyExistsException {
         return null;
