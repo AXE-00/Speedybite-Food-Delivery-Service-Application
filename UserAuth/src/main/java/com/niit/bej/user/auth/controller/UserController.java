242d2e39cad1f3fd25c1db3e4ba0da0755dab0f0
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-04 00:03:03 +0530

    added @RestController and @RequestMapping annotations to UserController.java

--- UserAuth/src/main/java/com/niit/bej/user/auth/controller/UserController.java
+++ UserAuth/src/main/java/com/niit/bej/user/auth/controller/UserController.java
@@ -1,4 +1,9 @@
 package com.niit.bej.user.auth.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
 public class UserController {
 }
