39a5f3081151e2e4bca98acc40a44c072c589786
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-03 23:09:37 +0530

    Initial commit

---
+++ UserAuth/src/main/java/com/niit/bej/user/auth/UserAuthApplication.java
@@ -0,0 +1,13 @@
+package com.niit.bej.user.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAuthApplication.class, args);
+    }
+
+}
---
+++ UserAuth/src/test/java/com/niit/bej/user/auth/UserAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.bej.user.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
