1036686184d8ae4b46c4603543ac50f914fec0c9
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-04 00:05:54 +0530

    injected field dependency of userService and securityTokenGenerator interface to UserController.java

--- UserAuth/src/main/java/com/niit/bej/user/auth/controller/UserController.java
+++ UserAuth/src/main/java/com/niit/bej/user/auth/controller/UserController.java
@@ -1,9 +1,20 @@
 package com.niit.bej.user.auth.controller;
 
+import com.niit.bej.user.auth.service.UserService;
+import com.niit.bej.user.auth.service.security.SecurityTokenGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/home")
 public class UserController {
+    private final UserService userService;
+    private final SecurityTokenGenerator securityTokenGenerator;
+
+    @Autowired
+    public UserController(UserService userService, SecurityTokenGenerator securityTokenGenerator) {
+        this.userService = userService;
+        this.securityTokenGenerator = securityTokenGenerator;
+    }
 }
