c5a415292f2e47d43307325d47031e250e21a9af
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-03 23:56:50 +0530

    implemented SecurityTokenGenerator interface

---
+++ UserAuth/src/main/java/com/niit/bej/user/auth/service/security/JwtSecurityTokenGeneratorImpl.java
@@ -0,0 +1,12 @@
+package com.niit.bej.user.auth.service.security;
+
+import com.niit.bej.user.auth.model.User;
+
+import java.util.Map;
+
+public class JwtSecurityTokenGeneratorImpl implements SecurityTokenGenerator {
+    @Override
+    public Map<String, String> generateToken(User user) {
+        return null;
+    }
+}
