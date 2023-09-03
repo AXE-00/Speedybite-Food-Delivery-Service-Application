42d214809df645d373a1121e36e579ffe4a4f1b8
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-03 23:33:18 +0530

    added findUserByEmail() to UserRepository.java

--- UserAuth/src/main/java/com/niit/bej/user/auth/repository/UserRepository.java
+++ UserAuth/src/main/java/com/niit/bej/user/auth/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.niit.bej.user.auth.repository;
 import com.niit.bej.user.auth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findUserByEmail(String email);
 }
