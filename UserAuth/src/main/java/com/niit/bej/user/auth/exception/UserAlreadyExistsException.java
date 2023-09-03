a245b43c296ae61c263e61f29398eaa088fac759
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-03 23:45:54 +0530

    added @ResponseStatus annotation to UserAlreadyExistsException.java

--- UserAuth/src/main/java/com/niit/bej/user/auth/exception/UserAlreadyExistsException.java
+++ UserAuth/src/main/java/com/niit/bej/user/auth/exception/UserAlreadyExistsException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.user.auth.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "User already exist in the database!", code = HttpStatus.CONFLICT)
 public class UserAlreadyExistsException extends Exception {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
 }
