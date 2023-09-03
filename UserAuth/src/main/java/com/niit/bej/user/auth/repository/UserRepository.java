a26f042dcd67c6ade0bfbf7c78e9906484caf6e0
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-03 23:32:44 +0530

    extended JpaRepository to UserRepository.java interface

--- UserAuth/src/main/java/com/niit/bej/user/auth/repository/UserRepository.java
+++ UserAuth/src/main/java/com/niit/bej/user/auth/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.niit.bej.user.auth.repository;
 
-public interface UserRepository {
+import com.niit.bej.user.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
 }
