10b2afefb0c2b2bc968cc6ae10a8f87e6eceeb8b
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-03 23:50:46 +0530

    added loginUser() to UserService.java interface

--- UserAuth/src/main/java/com/niit/bej/user/auth/service/UserService.java
+++ UserAuth/src/main/java/com/niit/bej/user/auth/service/UserService.java
@@ -1,8 +1,12 @@
 package com.niit.bej.user.auth.service;
 
+import com.niit.bej.user.auth.exception.InvalidCredentialsException;
 import com.niit.bej.user.auth.exception.UserAlreadyExistsException;
+import com.niit.bej.user.auth.exception.UserNotFoundException;
 import com.niit.bej.user.auth.model.User;
 
 public interface UserService {
     User registerUser(User user) throws UserAlreadyExistsException;
+
+    boolean loginUser(User user) throws InvalidCredentialsException, UserNotFoundException;
 }
