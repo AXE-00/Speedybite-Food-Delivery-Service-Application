cc7ebb2732fc30fae395074cd5be5658fcd961c4
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-08 21:37:53 +0530

    modified loginUser() and implemented updateUser() in UserServiceImpl.java

--- UserAuth/src/main/java/com/niit/bej/user/auth/service/UserServiceImpl.java
+++ UserAuth/src/main/java/com/niit/bej/user/auth/service/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package com.niit.bej.user.auth.service;
 
-import com.niit.bej.user.auth.exception.InvalidCredentialsException;
 import com.niit.bej.user.auth.exception.UserAlreadyExistsException;
 import com.niit.bej.user.auth.exception.UserNotFoundException;
 import com.niit.bej.user.auth.model.User;
@@ -20,28 +19,38 @@ public class UserServiceImpl implements UserService {
         this.userRepository = userRepository;
     }
 
+
     @Override
     public User registerUser(User user) throws UserAlreadyExistsException {
-        Optional<User> optionalUser = this.userRepository.findUserByEmail(user.getEmail());
+        Optional<User> optionalUser = userRepository.findUserByEmail(user.getEmail());
         if (optionalUser.isPresent()) {
-            throw new UserAlreadyExistsException("User With email Id: " + user.getEmail() + " already exists!");
+            throw new UserAlreadyExistsException("User with email ID: " + user.getEmail() + " already exists!");
         } else {
-            return this.userRepository.save(user);
+            return userRepository.save(user);
         }
     }
 
     @Override
-    public boolean loginUser(User user) throws InvalidCredentialsException, UserNotFoundException {
-        Optional<User> optionalUser = this.userRepository.findUserByEmail(user.getEmail());
-        if (optionalUser.isPresent()) {
-            User userInDatabase = optionalUser.get();
-            if (userInDatabase.getEmail().equals(user.getEmail()) && userInDatabase.getPassword().equals(user.getPassword())) {
-                return true;
-            } else {
-                throw new InvalidCredentialsException("Email or Password incorrect");
-            }
-        } else {
-            throw new UserNotFoundException("User with email Id: " + user.getEmail() + " does not exists!");
+    public User loginUser(User user) throws UserNotFoundException {
+        if (userRepository.findById(user.getEmail()).isPresent()) {
+            return userRepository.findByEmailAndPassword(user.getEmail(), user.getPassword());
+        } else throw new UserNotFoundException("User with email Id: " + user.getEmail() + " not found");
+    }
+
+
+    @Override
+    public User updateUser(String email, String password, String imageName, String phoneNumber) throws UserNotFoundException {
+        User user = userRepository.findById(email).orElseThrow(() -> new UserNotFoundException("User not found"));
+
+        if (password != null) {
+            user.setPassword(password);
         }
+        if (phoneNumber != null) {
+            user.setPhoneNumber(phoneNumber);
+        }
+        if (imageName != null) {
+            user.setImageName(imageName);
+        }
+        return userRepository.save(user);
     }
 }
