a9bb88f09d2604e85ee5f733e53c52baefa261b7
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-08 15:16:11 +0530

    added more fields and @AllArgsConstructor,@NoArgsConstructor and @Data annotations to User.java

--- UserAuth/src/main/java/com/niit/bej/user/auth/model/User.java
+++ UserAuth/src/main/java/com/niit/bej/user/auth/model/User.java
@@ -4,9 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.Objects;
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 @Table(name = "speedybite_users")
 public class User {
@@ -15,54 +19,9 @@ public class User {
     private String email;
     @Column(name = "password", length = 30, unique = true, nullable = false)
     private String password;
+    @Column(name = "role", length = 30, nullable = false)
+    private String role;
+    @Column(name = "imageName", length = 300, nullable = false)
+    private String imageName;
 
-    public User() {
-    }
-
-    public User(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        User user = (User) o;
-
-        if (!Objects.equals(email, user.email)) return false;
-        return Objects.equals(password, user.password);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = email != null ? email.hashCode() : 0;
-        result = 31 * result + (password != null ? password.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
