1635a6605f7a84e3b39b19c0b3495fcba58ca7e4
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-09 16:00:40 +0530

    added @ResponseStatus annotation; added a constructor in FavouriteItemNotFoundException.java

--- UserService/src/main/java/com/niit/bej/user/service/exception/FavouriteItemNotFoundException.java
+++ UserService/src/main/java/com/niit/bej/user/service/exception/FavouriteItemNotFoundException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.user.service.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "Favourite item not found for this id", code = HttpStatus.NOT_FOUND)
 public class FavouriteItemNotFoundException extends Exception {
+    public FavouriteItemNotFoundException(String message) {
+        super(message);
+    }
 }
