a31b4b5236a58296f6c12c6a10caf12295730a9d
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-09 15:49:34 +0530

    extended MongoRepository to UserRepository; added @Repository annotation to UserRepository.java interface

--- UserService/src/main/java/com/niit/bej/user/service/repository/UserRepository.java
+++ UserService/src/main/java/com/niit/bej/user/service/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.niit.bej.user.service.repository;
 
-public interface UserRepository {
+import com.niit.bej.user.service.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
 }
