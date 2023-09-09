594354c8b31745b64ed356246c24b6cbb1ae3fd8
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-09 15:13:11 +0530

    added @AllArgsConstructor,@NoArgsConstructor, @Data and @Id annotations to FavouriteCart.java

--- UserService/src/main/java/com/niit/bej/user/service/model/FavouriteCart.java
+++ UserService/src/main/java/com/niit/bej/user/service/model/FavouriteCart.java
@@ -1,6 +1,15 @@
 package com.niit.bej.user.service.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class FavouriteCart {
+    @Id
     private int itemId;
     private String itemName;
     private float itemPrice;
