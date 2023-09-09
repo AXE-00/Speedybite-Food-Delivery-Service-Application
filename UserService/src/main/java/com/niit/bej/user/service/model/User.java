477cf793eb5eef29e69370d30500fa3a649e0d7d
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-09 15:24:26 +0530

    added @AllArgsConstructor, @NoArgsConstructor, @Document, @Data, @JsonInclude, @Id and @JsonIgnoreProperties annotations to User.java

--- UserService/src/main/java/com/niit/bej/user/service/model/User.java
+++ UserService/src/main/java/com/niit/bej/user/service/model/User.java
@@ -1,8 +1,23 @@
 package com.niit.bej.user.service.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+@Data
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
+    @Id
     private String email;
     private String password;
     private String name;
