8129cc7ff2f95ee53cba9a0d18c05f25d0b78f68
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-09 15:46:57 +0530

    added fields to UserDto.java

--- UserService/src/main/java/com/niit/bej/user/service/model/UserDto.java
+++ UserService/src/main/java/com/niit/bej/user/service/model/UserDto.java
@@ -1,5 +1,10 @@
 package com.niit.bej.user.service.model;
 
 public class UserDto {
-
+    private String email;
+    private String password;
+    private String name;
+    private String role;
+    private String phoneNumber;
+    private String imageName;
 }
