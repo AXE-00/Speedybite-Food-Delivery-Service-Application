55a23f5c3b42af59ee24911c7778465fca8cac49
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-09 15:21:53 +0530

    added fields to User.java

--- UserService/src/main/java/com/niit/bej/user/service/model/User.java
+++ UserService/src/main/java/com/niit/bej/user/service/model/User.java
@@ -1,4 +1,15 @@
 package com.niit.bej.user.service.model;
 
+import java.util.List;
+
 public class User {
+    private String email;
+    private String password;
+    private String name;
+    private long phoneNumber;
+    private String role = "user";
+    private Address address;
+    private String city;
+    private byte[] imageName;
+    private List<FavouriteCart> favouriteCartList;
 }
