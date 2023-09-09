f38ce7b9a7bea8df99edd8907edc3979735b0124
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-09 15:57:22 +0530

    added addUser(), updateUser() and addFavouritesInList() method of type User in UserService.java

--- UserService/src/main/java/com/niit/bej/user/service/service/UserService.java
+++ UserService/src/main/java/com/niit/bej/user/service/service/UserService.java
@@ -1,4 +1,13 @@
 package com.niit.bej.user.service.service;
 
+import com.niit.bej.user.service.exception.UserNotFoundException;
+import com.niit.bej.user.service.model.FavouriteCart;
+import com.niit.bej.user.service.model.User;
+
 public interface UserService {
+    public User addUser(User user);
+
+    public User addFavouritesInList(String email, FavouriteCart favouriteCart);
+
+    public User updateUser(String email, User updatedUser) throws UserNotFoundException;
 }
