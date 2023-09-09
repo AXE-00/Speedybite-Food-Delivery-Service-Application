e08a99f214340c774afd7938bb8463d7e2d36dbd
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-09 15:05:54 +0530

    added fields to Address.java

--- UserService/src/main/java/com/niit/bej/user/service/model/Address.java
+++ UserService/src/main/java/com/niit/bej/user/service/model/Address.java
@@ -1,4 +1,11 @@
 package com.niit.bej.user.service.model;
 
+
 public class Address {
+    private String houseNumber;
+    private String landmark;
+    private String street;
+    private String city;
+    private String zipcode;
+
 }
