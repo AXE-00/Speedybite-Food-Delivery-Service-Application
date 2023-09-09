3e24c4a4f59b01b588d5eaa1d51f13cc5f0859b0
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-09 15:12:12 +0530

    added fields to FavouriteCart.java

--- UserService/src/main/java/com/niit/bej/user/service/model/FavouriteCart.java
+++ UserService/src/main/java/com/niit/bej/user/service/model/FavouriteCart.java
@@ -1,4 +1,9 @@
 package com.niit.bej.user.service.model;
 
 public class FavouriteCart {
+    private int itemId;
+    private String itemName;
+    private float itemPrice;
+    private float itemRating;
+    private String imageUrl;
 }
