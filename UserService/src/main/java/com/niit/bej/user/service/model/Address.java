bc696f03450c7512e9e4ac6b41b46da3fc6f54ab
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-09 15:09:32 +0530

    added @AllArgsConstructor,@NoArgsConstructor and @Data annotations to Address.java

--- UserService/src/main/java/com/niit/bej/user/service/model/Address.java
+++ UserService/src/main/java/com/niit/bej/user/service/model/Address.java
@@ -1,6 +1,12 @@
 package com.niit.bej.user.service.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Address {
     private String houseNumber;
     private String landmark;
