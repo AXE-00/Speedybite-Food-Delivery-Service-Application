e59c7544f4e213124222c7a4d8621a874b5b2ad8
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-11 12:06:12 +0530

    added proxy for registerUser() in UserProxy.java

--- UserService/src/main/java/com/niit/bej/user/service/proxy/UserProxy.java
+++ UserService/src/main/java/com/niit/bej/user/service/proxy/UserProxy.java
@@ -1,7 +1,13 @@
 package com.niit.bej.user.service.proxy;
 
+import com.niit.bej.user.service.model.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "user-auth-proxy")
 public interface UserProxy {
+    @PostMapping("home/register")
+    public ResponseEntity<?> registerUser(@RequestBody UserDto userDto);
 }
