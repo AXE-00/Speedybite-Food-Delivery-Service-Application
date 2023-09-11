f49dad0a48a862f4cecb3056d146cbe75121eacc
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-11 12:14:58 +0530

    added proxy for updateUser() in UserProxy.java

--- UserService/src/main/java/com/niit/bej/user/service/proxy/UserProxy.java
+++ UserService/src/main/java/com/niit/bej/user/service/proxy/UserProxy.java
@@ -3,11 +3,16 @@ package com.niit.bej.user.service.proxy;
 import com.niit.bej.user.service.model.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "user-auth-proxy")
 public interface UserProxy {
     @PostMapping("home/register")
     public ResponseEntity<?> registerUser(@RequestBody UserDto userDto);
+
+    @PutMapping("home/updateUser/{email}")
+    public ResponseEntity<?> updateUser(@RequestBody UserDto userDto, @PathVariable String email);
 }
