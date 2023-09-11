1e24725566dcf1e6b9fe1372371c96b05ad8acc7
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-11 11:37:18 +0530

    added EnableFeignClient andServletComponentScan  annotations to UserServiceApplication.java

--- UserService/src/main/java/com/niit/bej/user/service/UserServiceApplication.java
+++ UserService/src/main/java/com/niit/bej/user/service/UserServiceApplication.java
@@ -2,8 +2,12 @@ package com.niit.bej.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@ServletComponentScan
+@EnableFeignClients
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
