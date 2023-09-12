9266d7a45fba377db8d6173173536fdad96cf535
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-12 10:20:36 +0530

    injected constructor dependecy of userService in UserController.java

--- UserService/src/main/java/com/niit/bej/user/service/controller/UserController.java
+++ UserService/src/main/java/com/niit/bej/user/service/controller/UserController.java
@@ -1,10 +1,17 @@
 package com.niit.bej.user.service.controller;
 
+import com.niit.bej.user.service.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/userService")
 public class UserController {
+    private UserService userService;
 
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
