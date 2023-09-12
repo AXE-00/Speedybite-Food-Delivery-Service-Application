16784fc27952940891b4d05641a5668325f9ec43
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-12 13:11:57 +0530

    added updateCurrentUser() in UserController.java

--- UserService/src/main/java/com/niit/bej/user/service/controller/UserController.java
+++ UserService/src/main/java/com/niit/bej/user/service/controller/UserController.java
@@ -1,16 +1,15 @@
 package com.niit.bej.user.service.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.niit.bej.user.service.exception.UserNotFoundException;
 import com.niit.bej.user.service.model.User;
 import com.niit.bej.user.service.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -35,4 +34,19 @@ public class UserController {
         newUser.setImageName(newFileName);
         return new ResponseEntity<>(userService.addUser(newUser), HttpStatus.CREATED);
     }
+
+    @PutMapping("/update/user")
+    public ResponseEntity<?> updateCurrentUser(HttpServletRequest request, @RequestParam(value = "file", required = false) MultipartFile file, @RequestParam("userInfo") String user) throws IOException, UserNotFoundException {
+        String email = (String) request.getAttribute("email");
+        User newUser = new ObjectMapper().readValue(user, User.class);
+        if (file != null && !file.isEmpty()) {
+            newUser.setProfileImage(file.getBytes());
+            String filename = file.getOriginalFilename();
+            String newFileName = FilenameUtils.getBaseName(filename) + "_" + System.currentTimeMillis() + "." + FilenameUtils.getExtension(filename);
+            System.out.println(newFileName);
+            newUser.setImageName(newFileName);
+        }
+        return new ResponseEntity<>(userService.updateUser(email, newUser), HttpStatus.OK);
+    }
+
 }
