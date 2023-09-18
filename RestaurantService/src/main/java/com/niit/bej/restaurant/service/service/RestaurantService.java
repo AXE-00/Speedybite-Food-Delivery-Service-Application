7cf8cb2b433ddf9d42b92298f5c3ef88c2348bae
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 13:58:09 +0530

    added getItems(), updateRestaurant() and deleteFoodItem() in RestaurantService.java

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/service/RestaurantService.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/service/RestaurantService.java
@@ -1,7 +1,9 @@
 package com.niit.bej.restaurant.service.service;
 
+import com.niit.bej.restaurant.service.exception.FoodItemNotFoundException;
 import com.niit.bej.restaurant.service.exception.RestaurantAlreadyExistsException;
 import com.niit.bej.restaurant.service.exception.RestaurantNotFoundException;
+import com.niit.bej.restaurant.service.model.FoodItems;
 import com.niit.bej.restaurant.service.model.Restaurant;
 
 import java.util.List;
@@ -18,4 +20,10 @@ public interface RestaurantService {
     Restaurant updateRestaurant(Restaurant restaurant, int restaurantId) throws RestaurantNotFoundException;
 
     boolean deleteById(int restaurantId) throws RestaurantNotFoundException;
+
+    List<FoodItems> getItems(int id) throws RestaurantNotFoundException;
+
+    Restaurant updateFoodItem(int restaurantId, FoodItems newFoodItem) throws RestaurantNotFoundException, FoodItemNotFoundException;
+
+    boolean deleteFoodItem(int restaurantId, FoodItems foodItem) throws RestaurantNotFoundException, FoodItemNotFoundException;
 }
