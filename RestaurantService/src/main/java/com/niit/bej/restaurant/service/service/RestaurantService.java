44c102582c3a4a10a350f525cfe47791cfa78e23
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 13:41:01 +0530

    added getAllRestaurants(), getRestaurantByLocation() and getByRating() in RestaurantService.java

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/service/RestaurantService.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/service/RestaurantService.java
@@ -3,7 +3,14 @@ package com.niit.bej.restaurant.service.service;
 import com.niit.bej.restaurant.service.exception.RestaurantAlreadyExistsException;
 import com.niit.bej.restaurant.service.model.Restaurant;
 
+import java.util.List;
+
 public interface RestaurantService {
     Restaurant addRestaurant(Restaurant restaurant) throws RestaurantAlreadyExistsException;
 
+    List<Restaurant> getAllRestaurants();
+
+    Restaurant getRestaurantByLocation(String location);
+
+    Restaurant getByRating(double rating);
 }
