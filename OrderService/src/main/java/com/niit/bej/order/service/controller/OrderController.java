5ed0ad5201d06e2ff64451413dba9f4671e38a3a
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-19 01:42:59 +0530

    injected field dependency of orderService in OrderController.java

--- OrderService/src/main/java/com/niit/bej/order/service/controller/OrderController.java
+++ OrderService/src/main/java/com/niit/bej/order/service/controller/OrderController.java
@@ -1,10 +1,14 @@
 package com.niit.bej.order.service.controller;
 
+import com.niit.bej.order.service.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/order")
 public class OrderController {
+    @Autowired
+    OrderService orderService;
 
 }
