b05ecd24384aed1b8c93307a6d5871a6d6494357
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 21:47:07 +0530

    added addItem(), cancelOrder() and placeOrder() in OrderService.java

--- OrderService/src/main/java/com/niit/bej/order/service/service/OrderService.java
+++ OrderService/src/main/java/com/niit/bej/order/service/service/OrderService.java
@@ -11,4 +11,10 @@ public interface OrderService {
     List<Item> getItems(String status, String email);
 
     boolean removeItem(String email, Item item);
+
+    boolean addItem(String email, List<Item> items);
+
+    boolean cancelOrder(String email, List<Item> items);
+
+    boolean placeOrder(String email, List<Item> items);
 }
