940870fa25c396143bd625e22d079bb158076ba0
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 13:39:05 +0530

    added @ResponseStatus annotation to RestaurantAlreadyExistsException.java

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/exception/RestaurantAlreadyExistsException.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/exception/RestaurantAlreadyExistsException.java
@@ -1,4 +1,8 @@
 package com.niit.bej.restaurant.service.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "Restaurant with this Id already exists in database!")
 public class RestaurantAlreadyExistsException extends Exception {
 }
