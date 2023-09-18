c2f122b04f32c87f547aa9129214b2a6e843a398
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 18:21:44 +0530

    added addCuisine() in FoodItemsController.java

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/controller/FoodItemsController.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/controller/FoodItemsController.java
@@ -1,13 +1,27 @@
 package com.niit.bej.restaurant.service.controller;
 
+import com.niit.bej.restaurant.service.exception.RestaurantNotFoundException;
+import com.niit.bej.restaurant.service.model.FoodItems;
 import com.niit.bej.restaurant.service.service.FoodItemService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/cuisine")
 public class FoodItemsController {
     @Autowired
     FoodItemService foodItemService;
+
+    @PostMapping("/addNewCuisine/{restaurantId")
+    public ResponseEntity<?> addCuisine(HttpServletRequest httpServletRequest, @RequestBody FoodItems foodItems, @PathVariable int restaurantId) throws RestaurantNotFoundException {
+        if (httpServletRequest.getAttribute("email").equals("ashutosh.k.work@gmail.com")) {
+            return new ResponseEntity<>(foodItemService.addItems(foodItems, restaurantId), HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>("You are not authorized to add new items", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
 }
