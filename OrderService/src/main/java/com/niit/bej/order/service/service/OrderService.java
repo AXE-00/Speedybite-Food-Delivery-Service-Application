693d4bb0fb1110caa9c6c18085bfd4ed8c19efb4
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 21:45:22 +0530

    added addOrder(), getItems() and removeItem() in OrderService.java

--- OrderService/src/main/java/com/niit/bej/order/service/service/OrderService.java
+++ OrderService/src/main/java/com/niit/bej/order/service/service/OrderService.java
@@ -1,4 +1,14 @@
 package com.niit.bej.order.service.service;
 
+import com.niit.bej.order.service.model.Item;
+import com.niit.bej.order.service.model.Order;
+
+import java.util.List;
+
 public interface OrderService {
+    Order addOrder(Order order);
+
+    List<Item> getItems(String status, String email);
+
+    boolean removeItem(String email, Item item);
 }
