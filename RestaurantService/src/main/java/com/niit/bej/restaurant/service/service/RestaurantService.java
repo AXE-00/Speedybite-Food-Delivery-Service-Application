80a441d294b7b45ff07776164ddb5b616341010e
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 13:39:22 +0530

    added addRestaurant() to RestaurantService.java

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/service/RestaurantService.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/service/RestaurantService.java
@@ -1,4 +1,9 @@
 package com.niit.bej.restaurant.service.service;
 
+import com.niit.bej.restaurant.service.exception.RestaurantAlreadyExistsException;
+import com.niit.bej.restaurant.service.model.Restaurant;
+
 public interface RestaurantService {
+    Restaurant addRestaurant(Restaurant restaurant) throws RestaurantAlreadyExistsException;
+
 }
