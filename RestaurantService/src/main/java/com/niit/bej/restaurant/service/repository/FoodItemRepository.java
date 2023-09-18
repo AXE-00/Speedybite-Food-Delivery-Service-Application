4d09a7a574a83cb0edc1f850a1589cdd6f2badb0
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 13:28:04 +0530

    added findByItemName() in FoodItemRepository.java

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/repository/FoodItemRepository.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/repository/FoodItemRepository.java
@@ -1,8 +1,9 @@
 package com.niit.bej.restaurant.service.repository;
 
+import com.niit.bej.restaurant.service.model.FoodItems;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FoodItemRepository {
-
+    public FoodItems findByItemName(String itemName);
 }
