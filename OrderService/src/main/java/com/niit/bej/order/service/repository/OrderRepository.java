7a391865d346c858ba3815e6af4f1be9c3411d9a
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 21:40:08 +0530

    extended MongoRepository to OrderRepository.java

--- OrderService/src/main/java/com/niit/bej/order/service/repository/OrderRepository.java
+++ OrderService/src/main/java/com/niit/bej/order/service/repository/OrderRepository.java
@@ -1,4 +1,10 @@
 package com.niit.bej.order.service.repository;
 
-public interface OrderRepository {
+import com.niit.bej.order.service.model.Order;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends MongoRepository<Order, Integer> {
+
 }
