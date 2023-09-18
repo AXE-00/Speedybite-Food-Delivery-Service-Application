12dd34d9040e4620737106ba6a4c6628282df03a
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 13:52:57 +0530

    added updateRestaurant() and deleteById() in RestaurantService.java

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/service/RestaurantService.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/service/RestaurantService.java
@@ -1,6 +1,7 @@
 package com.niit.bej.restaurant.service.service;
 
 import com.niit.bej.restaurant.service.exception.RestaurantAlreadyExistsException;
+import com.niit.bej.restaurant.service.exception.RestaurantNotFoundException;
 import com.niit.bej.restaurant.service.model.Restaurant;
 
 import java.util.List;
@@ -13,4 +14,8 @@ public interface RestaurantService {
     Restaurant getRestaurantByLocation(String location);
 
     Restaurant getByRating(double rating);
+
+    Restaurant updateRestaurant(Restaurant restaurant, int restaurantId) throws RestaurantNotFoundException;
+
+    boolean deleteById(int restaurantId) throws RestaurantNotFoundException;
 }
