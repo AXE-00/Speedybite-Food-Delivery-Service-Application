09442ecaf15596e471503705affab59f6938126c
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 18:09:30 +0530

    injected field dependency of foodItemService in FoodItemsController.java

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/controller/FoodItemsController.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/controller/FoodItemsController.java
@@ -1,10 +1,13 @@
 package com.niit.bej.restaurant.service.controller;
 
+import com.niit.bej.restaurant.service.service.FoodItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/cuisine")
 public class FoodItemsController {
-
+    @Autowired
+    FoodItemService foodItemService;
 }
