5bbe5b04675699e5ac4e278a2386f09c691b30fa
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 21:06:16 +0530

    added @AllArgsConstructor, @NoArgsConstructor, @Data and @ToString annotations to Item.java

--- OrderService/src/main/java/com/niit/bej/order/service/model/Item.java
+++ OrderService/src/main/java/com/niit/bej/order/service/model/Item.java
@@ -1,4 +1,14 @@
 package com.niit.bej.order.service.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class Item {
+
 }
