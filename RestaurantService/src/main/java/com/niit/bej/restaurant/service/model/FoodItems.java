a2de7eeae6e0118ce7cb06a3b982b72726c8d993
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 13:17:34 +0530

    added @AllArgsConstructor, @NoArgsConstructor, @Data and @Document annotation to FoodItems.java

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/model/FoodItems.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/model/FoodItems.java
@@ -1,4 +1,14 @@
 package com.niit.bej.restaurant.service.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document
 public class FoodItems {
+
 }
