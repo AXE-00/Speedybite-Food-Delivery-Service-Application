9dd7a9c4849e671d185cbd6faf0497bcc600d446
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 15:48:37 +0530

    extended MongoRepository to RestaurantRepository.java and FoodItemRepository.java

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/repository/FoodItemRepository.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/repository/FoodItemRepository.java
@@ -1,9 +1,10 @@
 package com.niit.bej.restaurant.service.repository;
 
 import com.niit.bej.restaurant.service.model.FoodItems;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FoodItemRepository {
+public interface FoodItemRepository extends MongoRepository<FoodItems, Integer> {
     FoodItems findByItemName(String itemName);
 }
--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/repository/RestaurantRepository.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/repository/RestaurantRepository.java
@@ -1,10 +1,11 @@
 package com.niit.bej.restaurant.service.repository;
 
 import com.niit.bej.restaurant.service.model.Restaurant;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RestaurantRepository {
+public interface RestaurantRepository extends MongoRepository<Restaurant, Integer> {
     Restaurant findByLocation(String location);
 
     Restaurant findByRating(double rating);
