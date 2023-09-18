d5eee504a93e3bd546c5535a51f26bc381246ff1
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 20:50:07 +0530

    added filterUrl() in RestaurantServiceApplication.java

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/RestaurantServiceApplication.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/RestaurantServiceApplication.java
@@ -1,7 +1,10 @@
 package com.niit.bej.restaurant.service;
 
+import com.niit.bej.restaurant.service.filter.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestaurantServiceApplication {
@@ -10,4 +13,20 @@ public class RestaurantServiceApplication {
         SpringApplication.run(RestaurantServiceApplication.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean filterUrl() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new JwtFilter());
+
+        filterRegistrationBean.addUrlPatterns(
+                "/api/v1/restaurants/addRestaurant/*",
+                "/api/v1/restaurants/delete/*",
+                "/api/v1/restaurants/addNewCuisine/*",
+                "/api/v1/restaurants/update/*",
+                "/api/v1/restaurants/delete/*",
+                "/api/v1/restaurants/addItem/*",
+                "/api/v1/restaurants/updateItem/*",
+                "/api/v1/restaurants/deleteItem/*");
+        return filterRegistrationBean;
+    }
 }
