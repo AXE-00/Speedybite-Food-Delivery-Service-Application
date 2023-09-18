59217ba183744abf7718c794fbed79f9b35beb70
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 13:34:29 +0530

    added getAllItems() to FoodItemService.java

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/service/FoodItemService.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/service/FoodItemService.java
@@ -3,6 +3,10 @@ package com.niit.bej.restaurant.service.service;
 import com.niit.bej.restaurant.service.exception.RestaurantNotFoundException;
 import com.niit.bej.restaurant.service.model.FoodItems;
 
+import java.util.List;
+
 public interface FoodItemService {
     FoodItems addItems(FoodItems items, int restaurantId) throws RestaurantNotFoundException;
+
+    List<FoodItems> getAllItems();
 }
