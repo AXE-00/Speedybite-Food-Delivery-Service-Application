3c1cb6bdded9eaf2e870922aefb26806711d572e
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 13:33:52 +0530

    added addItems() in FoodItemService.java

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/service/FoodItemService.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/service/FoodItemService.java
@@ -1,4 +1,8 @@
 package com.niit.bej.restaurant.service.service;
 
+import com.niit.bej.restaurant.service.exception.RestaurantNotFoundException;
+import com.niit.bej.restaurant.service.model.FoodItems;
+
 public interface FoodItemService {
+    FoodItems addItems(FoodItems items, int restaurantId) throws RestaurantNotFoundException;
 }
