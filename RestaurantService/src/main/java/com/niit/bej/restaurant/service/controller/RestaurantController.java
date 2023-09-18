13ef791a4052bf55a14f85ba101603cc3c0263d4
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 19:31:54 +0530

    injected constructor dependency of restaurantRepository and restaurantService to RestaurantController.java

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/controller/RestaurantController.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/controller/RestaurantController.java
@@ -1,4 +1,21 @@
 package com.niit.bej.restaurant.service.controller;
 
+import com.niit.bej.restaurant.service.repository.RestaurantRepository;
+import com.niit.bej.restaurant.service.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/restaurant")
 public class RestaurantController {
+    RestaurantService restaurantService;
+    RestaurantRepository restaurantRepository;
+
+    @Autowired
+    public RestaurantController(RestaurantService restaurantService, RestaurantRepository restaurantRepository) {
+        this.restaurantService = restaurantService;
+        this.restaurantRepository = restaurantRepository;
+    }
+
 }
