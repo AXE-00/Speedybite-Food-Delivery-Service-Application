8fcf2b6bafa71a037a01b8831ce291b0ce3682f9
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 13:22:13 +0530

    added fields to Restaurant.java

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/model/Restaurant.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/model/Restaurant.java
@@ -2,11 +2,20 @@ package com.niit.bej.restaurant.service.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Data
 @Document
 public class Restaurant {
-
+    @Id
+    private int restaurantId;
+    private String restaurantName;
+    private String imageUrl;
+    private String location;
+    private double rating;
+    private List<FoodItems> items;
 }
