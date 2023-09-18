638cdad5547174335f48dfa350b9150700fe45c1
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 13:30:09 +0530

    added findByLocation() and findByRating() in RestaurantRepository.java

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/repository/RestaurantRepository.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/repository/RestaurantRepository.java
@@ -1,8 +1,11 @@
 package com.niit.bej.restaurant.service.repository;
 
+import com.niit.bej.restaurant.service.model.Restaurant;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestaurantRepository {
+    Restaurant findByLocation(String location);
 
+    Restaurant findByRating(double rating);
 }
