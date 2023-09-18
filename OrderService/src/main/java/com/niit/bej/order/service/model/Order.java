1c172ed730f8b5036cffd6f5242ccc86d17f906c
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 21:08:53 +0530

    added @Data, @AllArgsConstructor, @NoArgsConstructor, @ToString and @Document annotations to Order.java

--- OrderService/src/main/java/com/niit/bej/order/service/model/Order.java
+++ OrderService/src/main/java/com/niit/bej/order/service/model/Order.java
@@ -1,4 +1,16 @@
 package com.niit.bej.order.service.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Document
 public class Order {
+
 }
