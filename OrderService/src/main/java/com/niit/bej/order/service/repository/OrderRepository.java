f4962bc1f565da61adce6444fd63a04ffaed3373
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 21:41:58 +0530

    added findByCustomerId() in OrderRepository.java

--- OrderService/src/main/java/com/niit/bej/order/service/repository/OrderRepository.java
+++ OrderService/src/main/java/com/niit/bej/order/service/repository/OrderRepository.java
@@ -2,9 +2,13 @@ package com.niit.bej.order.service.repository;
 
 import com.niit.bej.order.service.model.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends MongoRepository<Order, Integer> {
-
+    @Query("{'customerId': ?0}")
+    List<Order> findByCustomerId(String email);
 }
