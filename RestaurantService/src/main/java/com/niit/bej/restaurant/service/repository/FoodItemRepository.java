c35ded3f6c85700244a6cff55df701867a91b2a2
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 13:29:22 +0530

    formatted code

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/repository/FoodItemRepository.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/repository/FoodItemRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FoodItemRepository {
-    public FoodItems findByItemName(String itemName);
+    FoodItems findByItemName(String itemName);
 }
