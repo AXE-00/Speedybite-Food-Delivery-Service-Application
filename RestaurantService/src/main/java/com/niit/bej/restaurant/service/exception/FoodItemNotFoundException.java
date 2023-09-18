b7529535f69d6532cf0a72b4ef29381e7a2b7068
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-18 13:56:19 +0530

    added @ResponseStatus annotation to FoodItemNotFoundException.java

--- RestaurantService/src/main/java/com/niit/bej/restaurant/service/exception/FoodItemNotFoundException.java
+++ RestaurantService/src/main/java/com/niit/bej/restaurant/service/exception/FoodItemNotFoundException.java
@@ -1,4 +1,8 @@
 package com.niit.bej.restaurant.service.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Food item with this Id does not exist in database!")
 public class FoodItemNotFoundException extends Exception {
 }
