953f42b2be4c76c1e0375f0cd51b255c43ed057d
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-19 13:00:44 +0530

    added orderFilter() in OrderServiceApplication.java

--- OrderService/src/main/java/com/niit/bej/order/service/OrderServiceApplication.java
+++ OrderService/src/main/java/com/niit/bej/order/service/OrderServiceApplication.java
@@ -1,7 +1,11 @@
 package com.niit.bej.order.service;
 
+import com.niit.bej.order.service.filter.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 
 @SpringBootApplication
 public class OrderServiceApplication {
@@ -10,4 +14,16 @@ public class OrderServiceApplication {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean<JwtFilter> orderFilter() {
+        FilterRegistrationBean<JwtFilter> bean = new FilterRegistrationBean<>(new JwtFilter());
+        bean.addUrlPatterns(
+                "/api/v1/order/insertOrder/*",
+                "/api/v1/order/placeOrder/*",
+                "/api/v1/order/cancelOrder/*",
+                "/api/v1/order/removeItem/*",
+                "/api/v1/order/addItem/*");
+        bean.setOrder(Ordered.LOWEST_PRECEDENCE);
+        return bean;
+    }
 }
