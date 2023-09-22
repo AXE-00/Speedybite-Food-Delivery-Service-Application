7b621e5b12c1305b49d5fbf07d9c124333909a69
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-22 11:37:35 +0530

    added routing to dashboard component and home component

--- FoodDeliveryAppFrontEnd/src/app/app-routing.module.ts
+++ FoodDeliveryAppFrontEnd/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {HeaderComponent} from "./components/header/header.component";
 
+const routes: Routes = [
+	{path: "", component: DashboardComponent},
+	{path: "header", component: HeaderComponent},
+];
 
 @NgModule({
-	declarations: [],
-	imports: [
-		CommonModule
-	]
+	imports: [RouterModule.forRoot(routes)], exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
