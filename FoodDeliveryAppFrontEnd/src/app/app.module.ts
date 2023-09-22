11cd2f4031a4e393198a38fce81d751b72548359
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-22 10:52:06 +0530

    generated app-routing.ts file

--- FoodDeliveryAppFrontEnd/src/app/app.module.ts
+++ FoodDeliveryAppFrontEnd/src/app/app.module.ts
@@ -4,6 +4,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouterOutlet} from "@angular/router";
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import {RouterOutlet} from "@angular/router";
 		BrowserModule,
 		BrowserAnimationsModule,
 		RouterOutlet,
+		AppRoutingModule,
 	],
   providers: [],
   bootstrap: [AppComponent]
