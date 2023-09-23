8f6806e5eaa155d3a3a4edebb9f09f564fb012ab
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-24 03:07:18 +0530

    modified homepage

--- FoodDeliveryAppFrontEnd/src/app/components/home/home.component.ts
+++ FoodDeliveryAppFrontEnd/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
 	selector: 'app-home',
@@ -6,5 +7,14 @@ import {Component} from '@angular/core';
 	styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+	present: boolean = true;
 
+	ngAfterViewInit(): void {
+		const autoTyped = new Typed('.autoType', {
+			strings: ['Tired of Cooking and Delivery Fees?', 'Movie Night? Date Night?', "Don't worry we got you covered!", "Your food is just one click away, Order Now!"],
+			typeSpeed: 100,
+			backSpeed: 10,
+			loop: false,
+		});
+	}
 }
