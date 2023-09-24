f38a991fe78d81cb6671df2047b096c92b91a53f
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-25 03:33:39 +0530

    added routing for aboutUs component in app-routing.module.ts

--- FoodDeliveryAppFrontEnd/src/app/app-routing.module.ts
+++ FoodDeliveryAppFrontEnd/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
 import {HeaderComponent} from "./components/header/header.component";
+import {AboutUsComponent} from "./components/about-us/about-us.component";
 
 const routes: Routes = [
 	{path: "", component: DashboardComponent},
 	{path: "header", component: HeaderComponent},
+	{path: "aboutUs", component: AboutUsComponent},
 ];
 
 @NgModule({
