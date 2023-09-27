6c1eedd164efa21db8e9cef4809cd1418f267ff1
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-27 16:07:58 +0530

    imported HttpClientModule and MatSnackBarModule in app.module.ts

--- FoodDeliveryAppFrontEnd/src/app/app.module.ts
+++ FoodDeliveryAppFrontEnd/src/app/app.module.ts
@@ -16,6 +16,8 @@ import {RegisterComponent} from './components/register/register.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
+import {HttpClientModule} from "@angular/common/http";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 @NgModule({
   declarations: [
@@ -37,7 +39,9 @@ import {MatIconModule} from "@angular/material/icon";
 		FormsModule,
 		ReactiveFormsModule,
 		MatButtonModule,
-		MatIconModule
+		MatIconModule,
+		HttpClientModule,
+		MatSnackBarModule
 	],
   providers: [],
   bootstrap: [AppComponent]
