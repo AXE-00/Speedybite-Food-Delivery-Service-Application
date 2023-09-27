844af924576ada522ad14ed0dce9b218c65a2728
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-28 03:04:42 +0530

    added routeLocator() in ApiGatewayConfig.java

--- APIGateway/src/main/java/com/niit/bej/api/gateway/config/ApiGatewayConfig.java
+++ APIGateway/src/main/java/com/niit/bej/api/gateway/config/ApiGatewayConfig.java
@@ -1,4 +1,28 @@
 package com.niit.bej.api.gateway.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ApiGatewayConfig {
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(routePredicate -> routePredicate.path("/api/v1/auth/**")
+                        .uri("http://localhost:8081"))
+                .route(routePredicate -> routePredicate.path("/api/v1/userService/**")
+                        .uri("http://localhost:8082"))
+                .route(routePredicate -> routePredicate.path("/api/v1/restaurant/**")
+                        .uri("http://localhost:8083"))
+                .route(routePredicate -> routePredicate.path("/api/v1/cuisine/**")
+                        .uri("http://localhost:8083"))
+                .route(routePredicate -> routePredicate.path("/api/v1/order/**")
+                        .uri("http://localhost:8084"))
+                .route(routePredicate -> routePredicate.path("/api/v1/payment/**")
+                        .uri("http://localhost:8085"))
+                .build();
+    }
+
 }
