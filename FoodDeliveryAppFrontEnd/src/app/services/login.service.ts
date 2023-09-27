618af7f19d60757d0fc45b5c63a9cb7335ff4a84
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-27 16:17:14 +0530

    added loginSuccess(), onFailure() and getLoginStatus() in login.service.ts

--- FoodDeliveryAppFrontEnd/src/app/services/login.service.ts
+++ FoodDeliveryAppFrontEnd/src/app/services/login.service.ts
@@ -1,10 +1,26 @@
-import {Injectable} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class LoginService {
 
+	isLoggedIn: boolean = false
+	searchElement: EventEmitter<string> = new EventEmitter();
+
+	//add eventEmitter for home and cart later
 	constructor() {
+	}
+
+	loginSuccess() {
+		this.isLoggedIn = true;
+	}
+
+	onFailure() {
+		this.isLoggedIn = false;
+	}
+
+	getLoginStatus() {
+		return this.isLoggedIn;
 	}
 }
