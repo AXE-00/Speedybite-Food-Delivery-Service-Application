debdbfe3117a5519c90cbb60a89ff7bed24ebdbd
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-27 16:09:26 +0530

    added login(), registerUser() and updateUser() in user.service.ts

--- FoodDeliveryAppFrontEnd/src/app/services/user.service.ts
+++ FoodDeliveryAppFrontEnd/src/app/services/user.service.ts
@@ -1,10 +1,38 @@
-import {Injectable} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class UserService {
 
-	constructor() {
+	authorizationUrl: String = "http://localhost:8081/home";
+	userUrl: String = "http://localhost:8081/api/v1/userService"
+	addressChanged: EventEmitter<boolean> = new EventEmitter();
+	public userInformation: any = {};
+
+	constructor(private httpClient: HttpClient) {
 	}
+
+	login(loginData: any): Observable<Object> {
+		console.log(loginData);
+		return this.httpClient.post(`${this.authorizationUrl}/login`, loginData);
+	}
+
+	registerUser(formData: FormData): Observable<any> {
+		return this.httpClient.post(`${this.userUrl}/register/user`, formData);
+	}
+
+	updateUser(formData: FormData): Observable<Object> {
+		let httpHeader: HttpHeaders = new HttpHeaders({
+			"Authorization": "Bearer " + localStorage.getItem('Token')
+		})
+		let requestOption = {headers: httpHeader}
+		console.log(requestOption)
+		console.log(formData)
+		return this.httpClient.put(`${this.userUrl}/update/user`, formData, requestOption)
+	}
+
 }
+
