df479c07847c8b3d5bfddb19cae4ce34ce6bcd3b
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-28 04:15:56 +0530

    removed @EnableEurekaClient annotation

--- OrderService/src/main/java/com/niit/bej/order/service/OrderServiceApplication.java
+++ OrderService/src/main/java/com/niit/bej/order/service/OrderServiceApplication.java
@@ -4,12 +4,10 @@ import com.niit.bej.order.service.filter.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
