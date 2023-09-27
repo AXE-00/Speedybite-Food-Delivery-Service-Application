f71ac9d03407eda8822f4e52a4131600601d787c
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-28 01:52:36 +0530

    added @RestController, @CrossOrigin and @RequestMapping annotations to PaymentController.java

---
+++ PaymentService/src/main/java/com/niit/bej/payment/service/controller/PaymentController.java
@@ -0,0 +1,12 @@
+package com.niit.bej.payment.service.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/v1/payment")
+public class PaymentController {
+
+}
