4fa38b8b72b4b25bbd0170e2bf2bf1e4475796f1
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-27 20:35:23 +0530

    added validateEmails(), validatePasswordMatch() and registerUser() in register.component.ts

--- FoodDeliveryAppFrontEnd/src/app/components/register/register.component.ts
+++ FoodDeliveryAppFrontEnd/src/app/components/register/register.component.ts
@@ -1,10 +1,108 @@
 import {Component} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {UserService} from "../../services/user.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {LoginService} from "../../services/login.service";
+import {Router} from "@angular/router";
 
 @Component({
-	selector: 'app-register',
-	templateUrl: './register.component.html',
-	styleUrls: ['./register.component.css']
+	selector: 'app-register', templateUrl: './register.component.html', styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+	responseData: any;
+	public profileImage: any = File;
+	registerForm: FormGroup = this.formBuilder.group({
+		email: ["", [Validators.required, this.validateEmails]],
+		password: ["", [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{7,}$/)]],
+		confirmPassword: ["", [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{7,}$/)]],
+		name: ["", [Validators.required, Validators.minLength(3), Validators.pattern('^[a-zA-Z]+$')]],
+		contactNumber: ["", [Validators.required, Validators.pattern('^[6-9]\\d{9}$')]]
+	}, {validators: [this.validatePasswordMatch]})
 
+	constructor(private formBuilder: FormBuilder, private userService: UserService, private snackBar: MatSnackBar, private loginStatus: LoginService, private router: Router) {
+	}
+
+	get email() {
+		return this.registerForm.get('email')
+	}
+
+	get password() {
+		return this.registerForm.get('password');
+	}
+
+	get confirmPassword() {
+		return this.registerForm.get('confirmPassword');
+	}
+
+	get name() {
+		return this.registerForm.get('name');
+	}
+
+	get contact() {
+		return this.registerForm.get('contactNumber');
+	}
+
+	validateEmails(control: AbstractControl) {
+		if (control.value !== '') {
+			const emailString = control.value;
+			const emails = emailString.split(',').map((email: string) => email.trim());
+			const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(gmail\.com|yahoo\.com)$/i;
+			const isValid = emails.every((email: string) => email.match(emailRegex) !== null);
+			if (!isValid) {
+				return {validateEmails: false}
+			}
+		}
+		return null;
+	}
+
+	validatePasswordMatch(formGroup: AbstractControl) {
+		const password = formGroup.get('password')?.value
+		const confirmPassword = formGroup.get('confirmPassword')?.value;
+		if (!password || !confirmPassword) {
+			return null;
+		}
+		if (password != confirmPassword) {
+			return {passwordDoNotMatch: true}
+		}
+		return null;
+	}
+
+	public onProfileImageSelect(event: any) {
+
+		const selectedImage = event.target.files[0];
+
+		console.log(selectedImage);
+
+		this.profileImage = selectedImage;
+
+	}
+
+	registerUser() {
+		const userData = this.registerForm.value;
+
+		const formData = new FormData();
+
+		formData.append('user', JSON.stringify(userData));
+
+		formData.append('profileImage', this.profileImage);
+
+		this.userService.registerUser(formData).subscribe({
+
+			next: data => {
+				const userName: string = String(this.name?.value);
+				localStorage.setItem('name', userName);
+
+				const message = `Welcome ${userName}, thank you for signing up with SpeedyBite`;
+
+				const email = JSON.stringify(this.registerForm.value.email);
+
+				this.snackBar.open('SignedUp successfully✔️', 'OK', {
+					duration: 3000, panelClass: ['mat-toolbar', 'blue']
+
+				});
+				this.loginStatus.loginSuccess();
+				this.router.navigateByUrl("/login")
+			}
+		})
+	}
 }
