6717b25754ae1d8f9b4cb681d7062a565706cd9a
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-27 16:08:29 +0530

    added routing to register component in app-routing.module.ts

--- FoodDeliveryAppFrontEnd/src/app/app-routing.module.ts
+++ FoodDeliveryAppFrontEnd/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from "@angular/router";
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
 import {HeaderComponent} from "./components/header/header.component";
 import {AboutUsComponent} from "./components/about-us/about-us.component";
+import {RegisterComponent} from "./components/register/register.component";
 
 const routes: Routes = [
 	{path: "", component: DashboardComponent},
 	{path: "header", component: HeaderComponent},
 	{path: "aboutUs", component: AboutUsComponent},
+	{path: "register", component: RegisterComponent},
 ];
 
 @NgModule({
