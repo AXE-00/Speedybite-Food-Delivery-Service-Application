e5948f641a697ad43f1e14fcce1654a9426fb28e
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-29 23:11:30 +0530

    created UserDto.java; added @AllArgsConstructor,@NoArgsConstructor and @Data annotation to UserDto.java

---
+++ UserAuth/src/main/java/com/niit/bej/user/auth/model/UserDto.java
@@ -0,0 +1,11 @@
+package com.niit.bej.user.auth.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDto {
+}
