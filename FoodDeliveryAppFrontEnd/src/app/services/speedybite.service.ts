3db43c4f3ad808f70c897a2c46e67cda2c9b9974
Ashutosh <ashutosh.k.work@gmail.com>
2023-10-01 01:42:34 +0530

    added getItems() and addItem() in speedybite.service.ts

--- FoodDeliveryAppFrontEnd/src/app/services/speedybite.service.ts
+++ FoodDeliveryAppFrontEnd/src/app/services/speedybite.service.ts
@@ -1,10 +1,33 @@
 import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Item} from "../models/item.model";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class SpeedybiteService {
+	private orderServiceUrl: String;
+	private paymentServiceUrl: String;
 
-	constructor() {
+
+	constructor(private httpClient: HttpClient) {
+		this.orderServiceUrl = 'http://localhost:9000/api/v1/order/'
+		this.paymentServiceUrl = 'http://localhost:9000/api/v1/payment/'
 	}
+
+	public getItems(status: String, email: String | null): Observable<Item[]> {
+		const url: string = `${this.orderServiceUrl}getItems/${status}/${email}`;
+		return this.httpClient.get<Item[]>(url);
+	}
+
+	public addItem(email: string | null, item: Item): Observable<any> {
+		let httpHeader: HttpHeaders = new HttpHeaders({
+			'Authorization': 'Bearer ' + localStorage.getItem('Token')
+		})
+		let requestOptions = {headers: httpHeader}
+		const url = `${this.orderServiceUrl}addItem/${email}`;
+		return this.httpClient.post<Item>(url, item, requestOptions);
+	}
+
 }
