a11af2d7cb9779a9529211aca57d58d1f950a355
Ashutosh <ashutosh.k.work@gmail.com>
2023-10-01 01:28:47 +0530

    added fields to favourites.model.ts

--- FoodDeliveryAppFrontEnd/src/app/models/favourites.model.ts
+++ FoodDeliveryAppFrontEnd/src/app/models/favourites.model.ts
@@ -1,2 +1,7 @@
 export class Favourites {
+	itemId: number | undefined;
+	itemName: string | undefined;
+	itemPrice: number | undefined;
+	itemRating: number | undefined;
+	imageUrl: string | undefined;
 }
