f9393ac422d6378013ef45760c6d259ae290e95a
Ashutosh <ashutosh.k.work@gmail.com>
2023-10-01 01:51:16 +0530

    added removeItem() and insertOrder() in speedybite.service.ts

--- FoodDeliveryAppFrontEnd/src/app/services/speedybite.service.ts
+++ FoodDeliveryAppFrontEnd/src/app/services/speedybite.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Item} from "../models/item.model";
+import {Order} from "../models/order.model";
 
 @Injectable({
 	providedIn: 'root'
@@ -28,6 +29,24 @@ export class SpeedybiteService {
 		let requestOptions = {headers: httpHeader}
 		const url = `${this.orderServiceUrl}addItem/${email}`;
 		return this.httpClient.post<Item>(url, item, requestOptions);
+	}
+
+	public removeItem(email: string | null, item: Item): Observable<any> {
+		let httpHeader = new HttpHeaders({
+			'Authorization': 'Bearer ' + localStorage.getItem('Token')
+		})
+		let requestOptions = {headers: httpHeader}
+		const url = `${this.orderServiceUrl}removeItem/${email}`;
+		return this.httpClient.post<Item>(url, item, requestOptions)
+	}
+
+	public insertOrder(order: Order): Observable<any> {
+		let httpHeader = new HttpHeaders({
+			'Authorization': 'Bearer ' + localStorage.getItem('Token')
+		})
+		let requestOptions = {headers: httpHeader}
+		const url = `${this.orderServiceUrl}insertOrder`
+		return this.httpClient.post<Order>(url, order, requestOptions)
 	}
 
 }
