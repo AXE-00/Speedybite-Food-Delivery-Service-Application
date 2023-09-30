ccf0a472b2b2248224c2c360016613a0d98e64af
Ashutosh <ashutosh.k.work@gmail.com>
2023-10-01 01:31:13 +0530

    added fields to item.model.ts

--- FoodDeliveryAppFrontEnd/src/app/models/item.model.ts
+++ FoodDeliveryAppFrontEnd/src/app/models/item.model.ts
@@ -1,2 +1,10 @@
 export class Item {
+	itemId: number | undefined
+	itemName: string | undefined
+	itemPrice: number | undefined
+	itemRating: number | undefined
+	imageUrl: string | undefined
+	count: number | undefined
+	status: string | undefined
+
 }
