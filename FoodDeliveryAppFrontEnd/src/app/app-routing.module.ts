aaf2311fe60ca58540bf7b3fb25480cdda268bf8
Ashutosh <ashutosh.k.work@gmail.com>
2023-10-01 02:03:04 +0530

    added routing to loginComponent in app-routing.module.ts

--- FoodDeliveryAppFrontEnd/src/app/app-routing.module.ts
+++ FoodDeliveryAppFrontEnd/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {DashboardComponent} from "./components/dashboard/dashboard.component";
 import {HeaderComponent} from "./components/header/header.component";
 import {AboutUsComponent} from "./components/about-us/about-us.component";
 import {RegisterComponent} from "./components/register/register.component";
+import {LoginComponent} from "./components/login/login.component";
 
 const routes: Routes = [
 	{path: "", component: DashboardComponent},
 	{path: "header", component: HeaderComponent},
 	{path: "aboutUs", component: AboutUsComponent},
 	{path: "register", component: RegisterComponent},
+	{path: "login", component: LoginComponent},
 ];
 
 @NgModule({
