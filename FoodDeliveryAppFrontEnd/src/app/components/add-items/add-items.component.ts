47de3c99a07c335d402c3f899a5bea90aac6c301
Ashutosh <ashutosh.k.work@gmail.com>
2023-10-04 19:38:25 +0530

    added addItem() and updateItem() methods in add-items.component.ts

--- FoodDeliveryAppFrontEnd/src/app/components/add-items/add-items.component.ts
+++ FoodDeliveryAppFrontEnd/src/app/components/add-items/add-items.component.ts
@@ -40,9 +40,9 @@ export class AddItemsComponent {
 		}
 	}
 
-	view() {
-		this.route.navigateByUrl('/viewAdminItem')
-	}
+	// view() {
+	// 	this.route.navigateByUrl('/adminItemView')
+	// }
 
 	updateItem() {
 		if (this.addItemForm.valid) {
