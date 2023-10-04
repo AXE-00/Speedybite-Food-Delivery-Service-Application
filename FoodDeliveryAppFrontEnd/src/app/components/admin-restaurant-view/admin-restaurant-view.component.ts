51dfb4f237211ffa9bb662fe6bb5bdcaf5e07663
Ashutosh <ashutosh.k.work@gmail.com>
2023-10-05 02:23:29 +0530

    added card to add restaurant

--- FoodDeliveryAppFrontEnd/src/app/components/admin-restaurant-view/admin-restaurant-view.component.ts
+++ FoodDeliveryAppFrontEnd/src/app/components/admin-restaurant-view/admin-restaurant-view.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {RestaurantService} from "../../services/restaurant.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
 	selector: 'app-admin-restaurant-view',
@@ -7,4 +9,46 @@ import {Component} from '@angular/core';
 })
 export class AdminRestaurantViewComponent {
 
+	@Input('restaurants') restaurants: any = [];
+
+	constructor(private restaurantService: RestaurantService, private route: Router, private activatedRoute: ActivatedRoute) {
+	}
+
+	ngOnInit() {
+		this.restaurantService.getAllRestaurant().subscribe(restaurants => {
+			this.restaurants = restaurants;
+		}, error => {
+			alert("error!!!!");
+		})
+		this.restaurantService.restaurantUpdated.subscribe(updated => {
+			if (updated == true) {
+				this.ngOnInit();
+			}
+		})
+	}
+
+	getItems(id: number) {
+		this.restaurantService.getId(id);
+		this.route.navigateByUrl('/adminFoodItem');
+	}
+
+	add() {
+		this.route.navigateByUrl('/addRestaurant');
+	}
+
+	deleteRestaurant(id: any) {
+		this.restaurantService.deleteRestaurant(id).subscribe(response => {
+			alert("restaurant is deleted");
+		}, error => {
+			alert("OOPS! only admin can delete a restaurant");
+		})
+		let arr = this.restaurants.filter((res: { restaurantId: any }) => {
+			if (res.restaurantId == id) {
+				return false;
+			} else {
+				return true;
+			}
+		})
+		this.restaurants = arr;
+	}
 }
