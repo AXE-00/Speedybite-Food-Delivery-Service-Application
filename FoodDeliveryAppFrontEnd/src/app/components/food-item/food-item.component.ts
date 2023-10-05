a64ff00f9673c88ea108193aba4e3a01a2aef753
Ashutosh <ashutosh.k.work@gmail.com>
2023-10-05 18:28:33 +0530

    added card for viewing added food items

--- FoodDeliveryAppFrontEnd/src/app/components/food-item/food-item.component.ts
+++ FoodDeliveryAppFrontEnd/src/app/components/food-item/food-item.component.ts
@@ -1,10 +1,30 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {RestaurantService} from "../../services/restaurant.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {LoginService} from "../../services/login.service";
 
 @Component({
-	selector: 'app-food-item',
-	templateUrl: './food-item.component.html',
-	styleUrls: ['./food-item.component.css']
+	selector: 'app-food-item', templateUrl: './food-item.component.html', styleUrls: ['./food-item.component.css']
 })
 export class FoodItemComponent {
 
+	@Input('restaurants') restaurants: any = [];
+
+	constructor(private restaurantService: RestaurantService, private loginService: LoginService, private route: Router, private activatedRoute: ActivatedRoute, private snackbar: MatSnackBar) {
+	}
+
+	ngOnInit(): void {
+		this.restaurantService.getAllRestaurant().subscribe(response => {
+			this.restaurants = response;
+		}, error => {
+			alert("Error!")
+		})
+	}
+
+	getItems(id: number) {
+		this.loginService.isLoggedIn;
+		this.restaurantService.getId(id);
+		this.route.navigateByUrl('/adminFoodItemsView')
+	}
 }
